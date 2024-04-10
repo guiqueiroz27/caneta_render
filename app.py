fc85fd883fb8505184ba2bd44a528a54761d8fb1
guiqueiroz27 <144967611+guiqueiroz27@users.noreply.github.com>
2024-04-09 23:38:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from oauth2client.service_account import ServiceAccountCredentials
 from flask import Flask, render_template
-from raspador import atualizar_planilha
+from raspador import atualizar_planilha()
 
 
 app = Flask(__name__)
@@ -24,14 +24,12 @@ def publicacoes():
 
 @app.route("/leissp")
 def leissp():
-    arquivo_credenciais = os.path.join(os.getcwd(), "insperaa-f16b8130bed9.json")
-
     # Chamando a função atualizar_planilha() para obter o HTML das tabelas
-    tabela1_html, tabela2_html = atualizar_planilha(arquivo_credenciais=arquivo_credenciais)
+    tabela1_html, tabela2_html = atualizar_planilha()
 
 
     # Passando o HTML das tabelas para o template leissp.html
     return render_template('leissp.html', tabela1_html=tabela1_html, tabela2_html=tabela2_html)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
